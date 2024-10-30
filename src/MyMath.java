6bdf9cab17d73257fce7d40377712c1abc16c62a
dakimov <danya.akimov.2016@gmail.com>
2024-10-30 19:05:11 +0300

    asd commit

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class MyMath {
     public static void main(String[] args) {
-        System.out.println(powerOfThree(243));
+        System.out.println(romanToInteger("MCMXCIV"));
     }
 
     public static List<String> fizzBuzz(int n) {
@@ -41,6 +43,27 @@ public class MyMath {
 
     public static boolean powerOfThree(int n) {
         return n > 0 && 1162261467 % n == 0;
+    }
+
+    public static int romanToInteger(String s) {
+        Map<Character, Integer> numsMap = new HashMap<>();
+        numsMap.put('I', 1);
+        numsMap.put('V', 5);
+        numsMap.put('X', 10);
+        numsMap.put('L', 50);
+        numsMap.put('C', 100);
+        numsMap.put('D', 500);
+        numsMap.put('M', 1000);
+        int result = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int c = numsMap.get(s.charAt(i));
+            if (i > 0 && numsMap.get(s.charAt(i - 1)) < c) {
+                c -= numsMap.get(s.charAt(i - 1));
+                i--;
+            }
+            result += c;
+        }
+        return result;
     }
 
 
